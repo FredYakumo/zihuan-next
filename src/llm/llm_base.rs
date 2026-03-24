2ab8fdb7df9a634650ffade1cfe9d8956b7d6b62
fredyakumo <fredyakumo@gmail.com>
2026-03-25 03:04:35 +0800

    split llm infer node and llm api node

--- src/llm/llm_base.rs
+++ src/llm/llm_base.rs
@@ -1,6 +1,6 @@
 use crate::llm::model::{InferenceParam, OpenAIMessage};
 
-pub trait LLMBase: std::fmt::Debug {
+pub trait LLMBase: std::fmt::Debug + Send + Sync {
     fn get_model_name(&self) -> &str;
 
     fn inference(&self, param: &InferenceParam) -> OpenAIMessage;
