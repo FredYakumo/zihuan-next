5aa4a9cf599d02d71ac0bc7fec96c517c8e5ad38
fredyakumo <fredyakumo@gmail.com>
2026-04-01 14:26:25 +0800

    Move tavily to rag

--- src/llm/mod.rs
+++ src/llm/mod.rs
@@ -8,8 +8,7 @@ pub mod llm_infer_node;
 pub mod model;
 pub mod natural_language_reply;
 pub mod prompt;
-pub mod tavily_provider_node;
-pub mod tavily_search_node;
+pub mod rag;
 pub mod tooling;
 pub mod util;
 
