59f6ea2ca455f06bc487a0363ea129a1cea457f6
Fred Yakumo <FredYakumo@gmail.com>
2026-06-23 16:26:27 +0800

    fix #22: media-id now is datetime-mediahash

--- zihuan_core/src/utils/hash_string.rs
+++ zihuan_core/src/utils/hash_string.rs
@@ -1,11 +1,14 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
+use chrono::Local;
+
 use crate::ims_bot_adapter::models::message::PersistedMediaSource;
 
 pub fn build_persisted_media_id(source: &PersistedMediaSource, original_source: &str, rustfs_path: &str) -> String {
     let seed = format!("{source}|{original_source}|{rustfs_path}");
     let mut hasher = DefaultHasher::new();
     seed.hash(&mut hasher);
-    format!("media-{:016x}", hasher.finish())
+    let now = Local::now().format("%Y-%m-%d-%H-%M-%S");
+    format!("{}-{:016x}", now, hasher.finish())
 }
