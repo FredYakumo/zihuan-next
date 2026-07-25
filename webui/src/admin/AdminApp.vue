1312e35ff51d11aa3142e4004ee5c52742b96389
fredyakumo <fredyakumo@gmail.com>
2026-07-26 02:11:34 +0800

    fix bug

--- webui/src/admin/AdminApp.vue
+++ webui/src/admin/AdminApp.vue
@@ -2,32 +2,66 @@
   <t-layout v-if="!isSetupRoute" class="admin-shell" :class="{ 'admin-shell--sidebar-open': sidebarOpen }">
     <t-aside class="admin-aside" :width="sidebarCollapsed ? '56px' : '216px'">
       <div class="admin-brand">
-        <img class="admin-brand-logo" :src="brandLogoSrc" alt="Zihuan Next" />
+        <img v-if="!sidebarCollapsed" class="admin-brand-logo" :src="brandLogoSrc" alt="Zihuan Next" />
         <span v-if="!sidebarCollapsed" class="admin-brand-title">Zihuan Next</span>
+        <t-button class="admin-collapse-toggle" variant="text" shape="square" @click="toggleSidebar">
+          <MenuUnfoldIcon v-if="sidebarCollapsed" />
+          <MenuFoldIcon v-else />
+        </t-button>
       </div>
       <t-menu :theme="menuTheme" :value="activeMenuValue" :collapsed="sidebarCollapsed" class="admin-menu">
-        <t-menu-item value="/" to="/" @click="closeSidebar">仪表盘</t-menu-item>
-        <t-menu-item value="/chat" to="/chat" @click="closeSidebar">对话</t-menu-item>
-        <t-menu-item value="/connections" to="/connections" @click="closeSidebar">连接配置</t-menu-item>
-        <t-menu-item value="/llm" to="/llm" @click="closeSidebar">模型配置</t-menu-item>
-        <t-menu-item value="/services" to="/services" @click="closeSidebar">Service 管理</t-menu-item>
-        <t-menu-item value="/graphs" to="/graphs" @click="closeSidebar">节点图与工作流</t-menu-item>
-        <t-menu-item value="/tasks" to="/tasks" @click="closeSidebar">任务管理器</t-menu-item>
+        <t-menu-item value="/" to="/" @click="closeSidebar">
+          <template #icon><DashboardIcon /></template>
+          仪表盘
+        </t-menu-item>
+        <t-menu-item value="/chat" to="/chat" @click="closeSidebar">
+          <template #icon><ChatIcon /></template>
+          对话
+        </t-menu-item>
+        <t-menu-item value="/connections" to="/connections" @click="closeSidebar">
+          <template #icon><LinkIcon /></template>
+          连接配置
+        </t-menu-item>
+        <t-menu-item value="/llm" to="/llm" @click="closeSidebar">
+          <template #icon><CloudIcon /></template>
+          模型配置
+        </t-menu-item>
+        <t-menu-item value="/services" to="/services" @click="closeSidebar">
+          <template #icon><ServerIcon /></template>
+          Service 管理
+        </t-menu-item>
+        <t-menu-item value="/graphs" to="/graphs" @click="closeSidebar">
+          <template #icon><SitemapIcon /></template>
+          节点图与工作流
+        </t-menu-item>
+        <t-menu-item value="/tasks" to="/tasks" @click="closeSidebar">
+          <template #icon><TaskIcon /></template>
+          任务管理器
+        </t-menu-item>
         <t-menu-item value="/logs" to="/logs" @click="closeSidebar">
+          <template #icon><ArticleIcon /></template>
           日志
           <t-tag v-if="logErrorBadgeEnabled && errorCount > 0" class="admin-nav-badge" theme="danger" variant="dark" size="small">
             {{ errorCount }}
           </t-tag>
         </t-menu-item>
-        <t-menu-item value="/commands" to="/commands" @click="closeSidebar">命令管理</t-menu-item>
-        <t-menu-item value="/connection-manager" to="/connection-manager" @click="closeSidebar">连接管理器</t-menu-item>
-        <t-menu-item value="/data-explorer" to="/data-explorer" @click="closeSidebar">数据检索</t-menu-item>
-        <t-menu-item value="/settings" to="/settings" @click="closeSidebar">设置</t-menu-item>
+        <t-menu-item value="/commands" to="/commands" @click="closeSidebar">
+          <template #icon><TerminalIcon /></template>
+          命令管理
+        </t-menu-item>
+        <t-menu-item value="/connection-manager" to="/connection-manager" @click="closeSidebar">
+          <template #icon><ControlPlatformIcon /></template>
+          连接管理器
+        </t-menu-item>
+        <t-menu-item value="/data-explorer" to="/data-explorer" @click="closeSidebar">
+          <template #icon><SearchIcon /></template>
+          数据检索
+        </t-menu-item>
+        <t-menu-item value="/settings" to="/settings" @click="closeSidebar">
+          <template #icon><SettingIcon /></template>
+          设置
+        </t-menu-item>
       </t-menu>
-      <t-button class="admin-collapse-toggle" variant="text" shape="square" @click="toggleSidebar">
-        <MenuUnfoldIcon v-if="sidebarCollapsed" />
-        <MenuFoldIcon v-else />
-      </t-button>
     </t-aside>
     <div v-if="showOverlay" class="admin-sidebar-overlay" @click="sidebarOpen = false"></div>
     <t-layout>
@@ -50,7 +84,22 @@
 <script setup lang="ts">
 import { computed, onMounted, onUnmounted, ref } from "vue";
 import { RouterView, useRoute } from "vue-router";
-import { MenuFoldIcon, MenuUnfoldIcon } from "tdesign-icons-vue-next";
+import {
+  ArticleIcon,
+  ChatIcon,
+  CloudIcon,
+  ControlPlatformIcon,
+  DashboardIcon,
+  LinkIcon,
+  MenuFoldIcon,
+  MenuUnfoldIcon,
+  SearchIcon,
+  ServerIcon,
+  SettingIcon,
+  SitemapIcon,
+  TaskIcon,
+  TerminalIcon,
+} from "tdesign-icons-vue-next";
 import brandLogoSrc from "../assets/brand-icon.png";
 import { useAdminApp } from "./composables/useAdminApp";
 import { errorCount, logErrorBadgeEnabled } from "./state/logStream";
